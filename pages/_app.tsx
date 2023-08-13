bc40cdb03db18debdc1e8aa4b5cdee1e72aaf375
Mano <mano.26q@gmail.com>
2023-08-13 14:31:10 +0530

    update title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,64 +7,54 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        {/* <title>Mano - Creative Development & Design</title>
-        <meta name="title" property="og:title" content="Mano" />
-        <meta
-          name="description"
-          content="Mano`s portfolio website. Built with NextJS and TailwindCSS. I love designing and building stuff on the web, and if you like cats, maybe we should be friends!"
-        />
-        <meta
-          name="description"
-          property="og:description"
-          content="Mano`s portfolio website. Built with NextJS and TailwindCSS. I love designing and building stuff on the web, and if you like cats, maybe we should be friends!"
-        />
-        <meta property="og:type" content="Portfolio Website" />
-        <meta name="author" content="Mano" /> */}
         <link rel="icon" href="/favicon.ico" />
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Mano - Creative Development & Design</title>
+        <title>Mano&apos;s Portfolio</title>
         <meta
           name="keywords"
           content="ui/ux, designer, freelance, website, portfolio"
         />
-        <meta name="author" content="Abhishek Jha" />
+        <meta name="author" content="Mano W" />
         <meta
           name="description"
-          content="Portfolio of Abhishek Jha. A UI/UX &amp; Visual designer from New Delhi."
+          content="Portfolio of Mano. A hobbyist coder from India."
         />
-        <meta property="og:title" content="Abhishek Jha — Visual Designer" />
+        <meta
+          property="og:title"
+          content="Mano - Creative Design and Development"
+        />
         <meta property="og:type" content="website" />
         <meta
           property="og:description"
-          content="Portfolio of Abhishek Jha. A UI/UX &amp; Visual designer who designs interactive websites &amp; apps."
+          content="Portfolio of Mano. A hobbyist coder from India."
         />
-        <meta property="og:url" content="https://abhishekjha.me/" />
+        {/* <meta property="og:url" content="" /> */}
         <meta
           property="og:image"
-          content="https://abhishekjha.me/Twitter.png"
+          content="https://raw.githubusercontent.com/Mano-08/Mano-08/orange-theme/id.png"
         />
-        <meta name="theme-color" content="#14CF93" />
+        <meta name="theme-color" content="#e8e8e8" />
         <meta
           name="twitter:card"
-          content="Abhishek Jha - UI/UX &amp; Visual Designer"
+          content="Mano - Creative Design and Development"
         />
-        <meta name="twitter:site" content="@designflavorist" />
-        <meta name="twitter:creator" content="@designflavorist" />
-        <meta name="twitter:url" content="https://abhishekjha.me" />
-        <meta name="twitter:title" content="2021 Portfolio of Abhishek Jha" />
+        <meta name="twitter:site" content="@mano__08" />
+        <meta name="twitter:creator" content="@mano__08" />
+        {/* <meta name="twitter:url" content="" /> */}
+        <meta name="twitter:title" content="Portfolio of Mano" />
         <meta
           name="twitter:description"
-          content="Abhishek Jha is a visual designer based in New Delhi. He makes high-end user interfaces."
+          content="Mano is a Software Developer from India. He is a hobbyist coder and loves to build cool web apps."
         />
         <meta
           name="twitter:image"
-          content="https://abhishekjha.me/Twitter.png"
+          content="https://raw.githubusercontent.com/Mano-08/Mano-08/orange-theme/id.png"
         />
         <meta
           name="twitter:image:alt"
-          content="Abhishek Jha Visual Designer."
+          content="Mano - Creative Design and Development."
         />
       </Head>
 
