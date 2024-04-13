537fec689873c923f08fd660ab1f25c2038610f7
Mano <mano.26q@gmail.com>
2024-04-13 12:49:53 +0530

    add battleship into project section

--- utils/images.ts
+++ utils/images.ts
@@ -1,9 +1,9 @@
 import idealcart from "../public/images/idealcart.png";
 import buynsell from "../public/images/buynsell.png";
-import recipeapp from "../public/images/recipeapp.png";
+import battleship from "../public/images/battleship.png";
 
 export const images: { [key: string]: any } = {
   idealcart: idealcart,
   buynsell: buynsell,
-  recipeapp: recipeapp,
+  battleship: battleship,
 };
